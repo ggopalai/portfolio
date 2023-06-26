a14d40ea848e7265be4a03a483290af02f878a65
Gagan Gowda <gagangowda26@gmail.com>
2023-06-25 22:14:35 -0700

    added kedarkantha images

--- pages/photography.js
+++ pages/photography.js
@@ -8,6 +8,9 @@ export default function Photography() {
         <div id={style.container}>
             <Head>Photography</Head>
             <Header />
+            <main id={style.main}>
+                This page is currently being built. Please check back later! In the meantime, you can check out my <a href="https://www.instagram.com/the_hippiemonk/" target="_blank">Instagram</a> for some of my photography.
+            </main>
             <Footer />
         </div>
     )
