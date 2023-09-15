898fd8d7900c4a6459883d3c2ec55254087929d8
Gagan Gowda <gagangowda26@gmail.com>
2023-09-14 23:58:01 -0700

    added about, updated resume, reduced headed padding

--- pages/index.js
+++ pages/index.js
@@ -25,12 +25,12 @@ export default function HomePage() {
                     
                     {/* intro text */}
                     <intro-text id={styles.introtext}>
-                        <p>Welcome to my website! I'm a graduate student at UC San Diego, currently pursuing a Masters in Computer Science with a specialization in Computer Systems. 
-                           My primary areas of interest lie in Cloud Computing, Distributed Systems, and Computer Networks.</p>
-                        <p>I am currently working with LitePoint Corp. as a SWE Cloud Intern. </p>
+                        <p>Welcome to my website! I'm a graduate student at UC San Diego, currently pursuing a Master's degree in Computer Science with a specialization in Computer Systems. 
+                           My primary areas of interest are Cloud Computing, Distributed Systems, and Computer Networks.</p>
+                        <p>I am currently interning with LitePoint Corp. as a SWE Cloud Intern. </p>
                         <p>Prior to joining UC San Diego, I gained valuable experience as a Software Engineer at Cisco, where I worked for 3 years. This role allowed me to contribute to various projects and expand my skills in software development, problem-solving, and collaboration within a tech-driven environment.</p>
-                        <p>I hold a bachelor's degree from R.V. College of Engineering in Bangalore, which provided me with a strong foundation in computer science.</p>
-                        <p>I am excited to showcase my portfolio and share my experiences with you. Feel free to explore and get in touch with me for any further inquiries or opportunities. Thank you for visiting!</p>
+                        <p>I hold a bachelor's degree from R.V. College of Engineering in Bangalore, which provided me with a solid foundation in computer science.</p>
+                        <p>Feel free to explore and get in touch with me for any further inquiries or opportunities. Thank you for visiting!</p>
                         <div id={styles.socialsContainer}>
                             < Socials />
                         </div>
@@ -46,9 +46,9 @@ export default function HomePage() {
                     <Card 
                         name="LitePoint"
                         position="Software Engineer Cloud Intern"
-                        exp={["Currently working on a project to remotely demo LitePoint's testers to potential customers.", 
-                              "Developing a full-stack application on Azure using Node.js and Express.js backend with a Next.js GUI, while managing deployments using Docker and Kubernetes.", 
-                              "Tech Stack - Microsoft Azure, Node.js, Express.js, React, HTML, CSS, JavaScript, Docker, Kubernetes, CosmosDB"]}
+                        exp={["Spearheaded the development of a web-application to remotely demo LitePoint's testers to potential customers.", 
+                              "The project directly contributed to the company's revenue by enabling the sales team to conduct demos with customers across the globe, thereby reducing the need for travel and in-person meetings.", 
+                              "Tech Stack - Microsoft Azure, Node.js, Express.js, React, HTML, CSS, JavaScript, Docker, Kubernetes, CosmosDB."]}
                         image="/logos/litepoint.svg"
                         start="June 2023"
                         end="Present"
@@ -58,11 +58,9 @@ export default function HomePage() {
                         name="Cisco Systems"
                         position="Software Engineer (Full Time)"
                         exp={[
-                                "Designed and implemented an Authentication/Authorization framework for the CX Cloud.",
-                                "Developed microservices using Java for the Optimal Software Version Recommendation team.",
-                                "Worked on projects with high impact in the Business Critical Services team.",
+                                "My significant contribution involves developing an Authentication/Authorization framework for the Cisco Customer Experience Cloud.",
+                                "Developed microservices in Python and Java for multiple customer-facing teams (SW Version Recommendation and Business Critical Services teams)",
                                 "Collaborated with the security team to ensure compliance with industry best practices.",
-                                "Gained exposure to working in a large-scale enterprise environment and handling the complexities associated with delivering high-quality software solutions.",
                                 "Tech Stack - Golang, Python, Java, Docker, Kubernetes, AWS, OAuth2.0, OPA, Okta"
                             ]}
                         image="/logos/cisco.png"
@@ -78,10 +76,10 @@ export default function HomePage() {
                     <Card 
                         name="Systems and Networking Group"
                         position="Graduate Research Assistant"
-                        exp={["I'm part of the Carbonless Cloud Computing lab, contributing to the GreenGrader project..", 
+                        exp={["I'm part of the Carbonless Cloud Computing lab, contributing to the Greengrader project.", 
                               "Our platform optimizes autograder container scheduling across cloud platforms with a focus on reducing carbon emissions.", 
                               "Our goal is to provide a sustainable solution for the autograding process, enabling educational institutions and organizations to efficiently utilize cloud resources while reducing their carbon footprint.",
-                              "Tech Stack - Python, Bash Scripting, Docker, Kubernetes, PostgreSQL, AWS"
+                              "Tech Stack - Python, Bash Scripting, Docker, Kubernetes, PostgreSQL, AWS, National Research Platform (NRP)"
                             ]}
                         image="/logos/ucsd.png"
                         start="April 2023"
@@ -94,10 +92,17 @@ export default function HomePage() {
                 <section id={styles.teaching}>
                     <h2>Teaching Experience</h2>
                     <Card 
+                        name="CSE 210 Principles of Software Engineering"
+                        position="Graduate Teaching Assistant"
+                        exp={["CSE 210 Principles of Software Engineering is a more focused and motivated version of CSE 110."]}
+                        image="/logos/ucsd.png"
+                        start="Sept 2023"
+                        end="Dec 2023"/>
+                    <Card 
                         name="CSE 110 Software Engineering"
                         position="Graduate Teaching Assistant"
-                        exp={["Served as a Teaching Assistant for CSE 110 Software Engineering, a course that focuses on the software development lifecycle and the various tools and techniques used in the process. As a TA, I was responsible for conducting weekly lab sessions, grading assignments, and assisting students with any questions or concerns they may have.",
-                                "This role has allowed me to gain valuable experience in teaching and mentoring students, as well as develop my communication and leadership skills."
+                        exp={["CSE 110 Software Engineering is a course that focuses on the software development lifecycle and the various tools and techniques used in the process. As a TA, I was responsible for mentoring a total of 11 teams (110 students in total) in their course projects.",
+                                "This role has allowed me to gain valuable experience in Engineering Management while teaching and mentoring students, as well as develop my communication and leadership skills."
                             ]}
                         image="/logos/ucsd.png"
                         start="Fall 2022"
