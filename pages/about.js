016fc9ed5e774da51db853987d9932c1d1d03f11
Gagan Gowda <gagangowda26@gmail.com>
2023-09-15 00:07:28 -0700

    updated index intro

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/About.module.css'
-import sideStyles from '../styles/Sidebar.module.css'
 import mainStyles from '../styles/Home.module.css'
-import Sidebar from '../components/sidebar';
 import Header from '../components/header';
 import Footer from '../components/footer';
 
